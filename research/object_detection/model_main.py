390b46e358684d6b57203a035eb5377af12d83ad
miChael suN <74409990+mcothin9@users.noreply.github.com>
2022-05-14 02:14:38 +1000

    Update model_main.py
    Reduce save checkpoints steps to save running time

--- research/object_detection/model_main.py
+++ research/object_detection/model_main.py
@@ -60,7 +60,7 @@ FLAGS = flags.FLAGS
 def main(unused_argv):
   flags.mark_flag_as_required('model_dir')
   flags.mark_flag_as_required('pipeline_config_path')
-  config = tf_estimator.RunConfig(model_dir=FLAGS.model_dir)
+  config = tf_estimator.RunConfig(model_dir=FLAGS.model_dir, save_checkpoints_steps=2000)
 
   train_and_eval_dict = model_lib.create_estimator_and_inputs(
       run_config=config,
